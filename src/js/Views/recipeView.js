c09384407f20d6f292708852ccb1146725b403f6
isko82mk <janevski.ivica@yahooo.com>
2024-02-11 13:47:32 +0100

    model and recipeView created

--- src/js/Views/recipeView.js
+++ src/js/Views/recipeView.js
@@ -1,3 +1,115 @@
-class RecipeView {}
+import icon from '../../img/icons.svg';
+import fracty from 'fracty';
+
+class RecipeView {
+  _data;
+  _parentElement = document.querySelector('.recipe');
+
+  render(data) {
+    this._data = data;
+    const markup = this._generateMarkup();
+    this._parentElement.innerHTML = '';
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  _generateMarkup() {
+    return `   <figure class="recipe__fig">
+    <img src="${this._data.imageUrl}" alt="Tomato" class="recipe__img" />
+    <h1 class="recipe__title">
+      <span>${this._data.title}</span>
+    </h1>
+  </figure>
+
+  <div class="recipe__details">
+    <div class="recipe__info">
+      <svg class="recipe__info-icon">
+        <use href="${icon}.svg#icon-clock"></use>
+      </svg>
+      <span class="recipe__info-data recipe__info-data--minutes">${
+        this._data.cookingTime
+      }</span>
+      <span class="recipe__info-text">minutes</span>
+    </div>
+    <div class="recipe__info">
+      <svg class="recipe__info-icon">
+        <use href="${icon}.svg#icon-users"></use>
+      </svg>
+      <span class="recipe__info-data recipe__info-data--people">${
+        this._data.servings
+      }</span>
+      <span class="recipe__info-text">servings</span>
+
+      <div class="recipe__info-buttons">
+        <button class="btn--tiny btn--increase-servings">
+          <svg>
+            <use href="${icon}.svg#icon-minus-circle"></use>
+          </svg>
+        </button>
+        <button class="btn--tiny btn--increase-servings">
+          <svg>
+            <use href="${icon}#icon-plus-circle"></use>
+          </svg>
+        </button>
+      </div>
+    </div>
+
+    <div class="recipe__user-generated">
+      <svg>
+        <use href="${icon}.svg#icon-user"></use>
+      </svg>
+    </div>
+    <button class="btn--round">
+      <svg class="">
+        <use href="${icon}.svg#icon-bookmark-fill"></use>
+      </svg>
+    </button>
+  </div>
+
+  <div class="recipe__ingredients">
+    <h2 class="heading--2">Recipe ingredients</h2>
+    <ul class="recipe__ingredient-list">
+    ${this._data.ingredients
+      .map(data => this._generateIngredientsMarkup(data))
+      .join('')}
+    </ul>
+  </div>
+
+  <div class="recipe__directions">
+    <h2 class="heading--2">How to cook it</h2>
+    <p class="recipe__directions-text">
+      This this._data was carefully designed and tested by
+      <span class="recipe__publisher">The Pioneer Woman</span>. Please check out
+      directions at their website.
+    </p>
+    <a
+      class="btn--small recipe__btn"
+      href="http://thepioneerwoman.com/cooking/pasta-with-tomato-cream-sauce/"
+      target="_blank"
+    >
+      <span>Directions</span>
+      <svg class="search__icon">
+        <use href="src/img/icons.svg#icon-arrow-right"></use>
+      </svg>
+    </a>
+  </div>`;
+  }
+
+  _generateIngredientsMarkup(data) {
+    return `
+    <li class="recipe__ingredient">
+    <svg class="recipe__icon">
+      <use href="${icon}.svg#icon-check"></use>
+    </svg>
+    <div class="recipe__quantity">${
+      data.quantity === null ? '' : fracty(data.quantity)
+    }</div>
+    <div class="recipe__description">
+      <span class="recipe__unit"${data.unit}</span>
+      ${data.description}
+    </div>
+  </li>
+`;
+  }
+}
 
 export default new RecipeView();
--- src/js/model.js
+++ src/js/model.js
@@ -1,3 +1,23 @@
-const state = {
+import { API_URL } from './config';
+
+export const state = {
   recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  const res = await fetch(`${API_URL}/${id}`);
+  const data = await res.json();
+
+  const { recipe } = data.data;
+
+  state.recipe = {
+    id: recipe.id,
+    cookingTime: recipe.cooking_time,
+    imageUrl: recipe.image_url,
+    ingredients: recipe.ingredients,
+    publisher: recipe.publisher,
+    servings: recipe.servings,
+    sourceUrl: recipe.source_url,
+    title: recipe.title,
+  };
 };
