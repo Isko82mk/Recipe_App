d804b742c115fd7207ac400904ada0daf7706936
isko82mk <janevski.ivica@yahooo.com>
2024-02-11 14:11:58 +0100

    config change

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export default API_URL = 'https://forkify-api.herokuapp.com/v2';
-=======
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
->>>>>>> new-feature
